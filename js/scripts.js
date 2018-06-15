6f057e52fe5d474c51d7475c787012f2b0bfac71
protatodev <thaddonaghue@yahoo.com>
2018-06-15 08:19:02 -0700

    add initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Pizza() {
+  this.size = 0;
+  this.toppings = [];
+}
+
+Pizza.prototype.getToppings() {
+
+}
+
+Pizza.prototype.calculateCost() {
+
+}
+
+function buildOrder(pizza) {
+  var order
+}
